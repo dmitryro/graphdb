899a1450275e2dc1651751e78ba39694f965c98a
Dmitry Roitman <dmitryro@gmail.com>
2025-02-15 11:31:32 -0500

    Refactored into modular workspace - server, proto, client, rest_api, plugins, lib

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,10 +1,8 @@
 pub mod query_parser; // Ensure this line exists
-pub mod cli;
 pub mod graph_evolution;
 pub mod network_interfaces;
 pub mod plugin_system;
 pub mod query_exec_engine;
-pub mod rest_api;
 pub mod storage_engine;
 pub mod transact_indexing;
 pub mod indexing_caching; // Import the module
---
+++ proto/build.rs
@@ -0,0 +1,19 @@
+use std::error::Error;
+use std::process::exit;
+
+fn run() -> Result<(), Box<dyn Error>> {
+    tonic_build::configure()
+        // .format(false) // disable code formatting since docs.rs will otherwise break
+        .compile_protos(&["graphdb.proto"], &["."])?;
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("{err}");
+        exit(1);
+    }
+
+    // Tells cargo to only rebuild if the proto file changed
+    println!("cargo:rerun-if-changed=graphdb.proto");
+}
